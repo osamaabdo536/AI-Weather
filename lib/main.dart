af7a87fb679d2a5b1df48980d7c95bd1093bbeb1
osamaabdo536 <93677924+osamaabdo536@users.noreply.github.com>
2025-02-11 21:04:27 +0200

    initial project.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
