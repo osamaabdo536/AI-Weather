12fd4194d2e69f4dd3f61d97451f66b8fee3bab4
osamaabdo536 <93677924+osamaabdo536@users.noreply.github.com>
2025-02-12 14:25:06 +0200

    Linked the app with Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:weather/core/Router/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'firebase_options.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const WeatherApp());
 }
 
